28f027881896f722244d672282d51f2afba6940a
Dusmar6 <36971079+Dusmar6@users.noreply.github.com>
2019-03-29 18:28:05 -0700

    Update FileEncryption.py

--- File Encryption/FileEncryption.py
+++ File Encryption/FileEncryption.py
@@ -3,30 +3,33 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding, hashes, hmac
 from os import urandom
 
-folderPath = "/Users/SadiqSarwar/Desktop/Lake.jpg"
-encPath = "/Users/SadiqSarwar/Desktop/Lake[Encrypted].enc"
-decPath = "/Users/SadiqSarwar/Desktop/Lake[Decypted].jpg"
-ext = ".jpg"
+folderPath = ##path for file you'd like to encrypt
+encPath = ##folder to place encrypted file
+decPath = ##path to file you'd like to decrypt
+ext = #file extension
+
 ivLen = 16
 keyLen = 32
 padLen = 128
 hmacLen = 256
 
-
+##menua
 def menu():
     print(" --[Menu]-- ")
     print("1. Encrypt")
     print("2. Decrypt")
     print("3. Exit")
 
+    ##opens file, returns byte array
 def read(filepath):
     return open(filepath, "rb").read()
 
+## performs all encryption calculations with CBC mac
 def myEncryptMAC(file, key, hkey):
     iv = urandom(ivLen)
     backend = default_backend()
     padder = padding.PKCS7(padLen).padder()
-    paddedFile = padder.update(file)
+    paddedFile = padder.update(file)## pads the file
     paddedFile += padder.finalize()
     encryption = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
     encryptor = encryption.encryptor()
@@ -38,6 +41,7 @@ def myEncryptMAC(file, key, hkey):
     enc.write(cipherText)
     return cipherText, iv, tag
 
+## opens file and sets up encryption with CBC Mac
 def myFileEncryptMAC():
     filePath = folderPath
     file = read(filePath)
@@ -46,6 +50,7 @@ def myFileEncryptMAC():
     encryptedSet = myEncryptMAC(file, key, hkey)
     return encryptedSet[0], encryptedSet[1], encryptedSet[2], key, hkey, ext
 
+##performs CBCMAC decryption
 def myDecryptMAC(file, key, iv, tag, hkey):
     backend = default_backend()
     hashfunc = hmac.HMAC(hkey, hashes.SHA256(), backend = default_backend())
@@ -60,6 +65,7 @@ def myDecryptMAC(file, key, iv, tag, hkey):
     plainText += unpadder.finalize()
     return plainText
 
+##opens file and sets up CBCMAC decrypt
 def myFileDecryptMAC(encrypInfo):
     file = read(encPath)
     decryptedSet = myDecryptMAC(file, encrypInfo[3],encrypInfo[1],encrypInfo[2],encrypInfo[4])
@@ -68,6 +74,7 @@ def myFileDecryptMAC(encrypInfo):
     
 ### BELOW AES METHOD WITHOUT HASH
 
+##performs cbc encryption on file
 def myEncrypt(file, key):
     iv = urandom(ivLen)
     backend = default_backend()
@@ -81,6 +88,7 @@ def myEncrypt(file, key):
     enc.write(cipherText)
     return cipherText, iv
 
+##sets up cbc encryption
 def myFileEncrypt():
     filePath = folderPath
     file = read(filePath)
@@ -88,6 +96,7 @@ def myFileEncrypt():
     encryptedSet = myEncrypt(file, key)
     return encryptedSet[0], encryptedSet[1], key, ext
 
+##performs cbc decrytpion
 def myDecrypt(file, key, iv):
     backend = default_backend()
     decryption = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
@@ -98,13 +107,14 @@ def myDecrypt(file, key, iv):
     plainText += unpadder.finalize()
     return plainText
 
-
+##sets up zes cbc decryption
 def myFileDecrypt(encrypInfo):
     file = read(encPath)
     decryptedSet = myDecrypt(file, encrypInfo[2],encrypInfo[1])
     dec = open(decPath, "wb")
     dec.write(decryptedSet)
 
+    ##main menu logic
 def main():
     encrypInfo = None
     cont = True
