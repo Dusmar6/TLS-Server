a1df6abdf67328855fe551ee526e0461804bede9
Dusmar6 <36971079+Dusmar6@users.noreply.github.com>
2019-04-17 21:52:31 -0700

    Update RSAFileEncryption.py

--- RSA File/RSAFileEncryption.py
+++ RSA File/RSAFileEncryption.py
@@ -8,13 +8,18 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 
 from os import urandom
 from os import path
+import os.path
 
-folderPath = "/Users/SadiqSarwar/Desktop/Lake.jpg"
-encPath = "/Users/SadiqSarwar/Desktop/Lake[Encrypted].enc"
-decPath = "/Users/SadiqSarwar/Desktop/Lake[Decypted].jpg"
-RSA_publicKey_filepath = "/Users/SadiqSarwar/Desktop/RSA_Public_Key.pem"
-RSA_privateKey_filepath = "/Users/SadiqSarwar/Desktop/RSA_Private_Key.pem"
+import json
+
+directory = "C:\\Users\\dusma\\Desktop\\CryptoFiles\\"
+folderPath = "C:\\Users\\dusma\\Desktop\\CryptoFiles\\image.jpg"
+encPath = "C:\\Users\\dusma\\Desktop\\CryptoFiles\\image[enc].jpg"
+decPath = "C:\\Users\\dusma\\Desktop\\CryptoFiles\\image[dec].jpg"
+RSA_publicKey_filepath = "C:\\Users\\dusma\\Desktop\\RSA_Public_Key.pem"
+RSA_privateKey_filepath = "C:\\Users\\dusma\\Desktop\\RSA_Private_Key.pem"
 ext = ".jpg"
+
 ivLen = 16
 keyLen = 32
 padLen = 128
@@ -26,19 +31,19 @@ def menu():
     print("1. Encrypt")
     print("2. Decrypt")
     print("3. Exit")
-
+    
+    '''
+    #Generate key
+    #public_exponent = Fermat Prime # to use as exponent for new key. Recommended: 65537
+    #key_size = Bit length of key
+    #backend = Implements RSABackend
+    '''
 def retrieveRSAKeys():
-    #Check to see if PEM files exists
     PrivateKeyExists = path.isfile(RSA_privateKey_filepath)
     PublicKeyExists = path.isfile(RSA_publicKey_filepath)
     
-    #Retrieve the PEM files if they do exist and return them
     if PrivateKeyExists and PublicKeyExists:
         print("[File was found in path - Keys have been loaded]")
-        
-        #Using Deserialization to open a stream of bytes from file as an object.
-        #key_file.read() is reading the data from the file and passing it in as the PEM encoded data
-        #password, if used is to decrypt the data. 
         with open(RSA_privateKey_filepath, "rb") as key_file:
             privateKey = serialization.load_pem_private_key(key_file.read(),
                                                              password=None,
@@ -46,26 +51,19 @@ def retrieveRSAKeys():
         with open(RSA_publicKey_filepath, "rb") as key_file:
             publicKey = serialization.load_pem_public_key(key_file.read(),
                                                              backend=default_backend())    
+            
         return publicKey, privateKey
     
-    else: #If they do not exist, Create the files
+    else:
         print("[File was not found in path - Generating new keys]")
-        #Generate Private KeyGenerate key
-        #public_exponent = Biggest Fermat Prime # using conjecture 2^2n +1 to use as exponent for new key. Recommended: 65537
-        #If p is prime, then a^p is congruent to a modulo p - Needed for the proof of correctness in RSA.
-        #key_size = Bit length of key
-        #backend = Implements RSABackend
+        #Generate Private Key
         privateKey = rsa.generate_private_key(public_exponent = 65537,
                                               key_size = 4096,
                                               backend = default_backend())
         #Encode Private Key into PEM File
-        #Using Serialization to convert object to a stream of bytes to store as a file.
-        #TraditionalOpenSSL using PKCS#1 Format
         pem = privateKey.private_bytes(encoding = serialization.Encoding.PEM,
                                        format = serialization.PrivateFormat.TraditionalOpenSSL,
                                        encryption_algorithm = serialization.NoEncryption())
-        
-        #Returns list of lines in private key, breaking at line boundries
         pem.splitlines()[0]
         
         #Open File Path for Private Key
@@ -79,11 +77,8 @@ def retrieveRSAKeys():
         #Generate Public Key
         publicKey = privateKey.public_key()
         #Enclode Public Key into PEM file
-        #SubjectPublicKeyInfo saves public key as string so it can be read
         pem = publicKey.public_bytes(encoding = serialization.Encoding.PEM,
                                      format = serialization.PublicFormat.SubjectPublicKeyInfo)
-        
-        #Returns list of lines in private key, breaking at line boundries
         pem.splitlines()[0]
         
         #Open File Path for Private Key
@@ -98,9 +93,10 @@ def retrieveRSAKeys():
     
     
     
-def myRSAEncrypt():
+def myRSAEncrypt(filepath):
+    
     #Load Encryption Information
-    encrypInfo = myFileEncryptMAC()
+    encrypInfo = myFileEncryptMAC(filepath)
     
     #Load from RSA Keys from File Path
     RSA_KeyInfo = retrieveRSAKeys()
@@ -108,17 +104,33 @@ def myRSAEncrypt():
     privateKey = RSA_KeyInfo[1]
     
     #Encrypting Key Variable
-    #OAEP through SHA256 hash and MGF1 hash, which takes arbitray to fixed and arbitrary output. 
-    key = encrypInfo[3] + encrypInfo[4] #concatinate the keys
+    key = encrypInfo[3] + encrypInfo[4] #key + hkey concatinated
+
     RSA_CipherText = publicKey.encrypt(key, paddings.OAEP(
             mgf = paddings.MGF1(algorithm = hashes.SHA256()),
             algorithm = hashes.SHA256(),
             label = None))
+    
+    #new enc file path
+    temp = filepath[0:-4]
+    encDest = temp + "[enc].json"
+    
+    #write json file
+    with open(encDest,"w") as write_file:
+        json.dump(list(RSA_CipherText), write_file)
+    #directly dumping a byte array throws an error (bytes are unserializable with JSON), so we have to dump a list
+    #if we were directly decrypting the file, we would have to convert back from list to byte array 
+    #but for now we are decrypting using the returned ciphertext
+        
+    #removes og file
+    os.remove(filepath)
+    
     print("RSA Encryption Complete")
-    return RSA_CipherText, encrypInfo[0], encrypInfo[1], encrypInfo[2], ext
+    return RSA_CipherText, encrypInfo[0], encrypInfo[1], encrypInfo[2], encrypInfo[5], filepath
 
 
-def myRSADecrypt(RSA_CipherText, C, IV, tag, ext):
+
+def myRSADecrypt(RSA_CipherText, C, IV, tag, ext, filepath):
     #Load from RSA Keys from File Path
     RSA_KeyInfo = retrieveRSAKeys()
     publicKey = RSA_KeyInfo[0]
@@ -131,8 +143,18 @@ def myRSADecrypt(RSA_CipherText, C, IV, tag, ext):
             mgf=paddings.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
             label=None))
+    
+    #naming decrypted file
+    decDest = filepath[0:-4] + "[dec]" + filepath[-4:]
+    
+    #writing decrypted file
+    dec = open(decDest, "wb")
+    dec.write(RSA_PlainText )
+    
     print("RSA Decryption Complete")
     return RSA_PlainText
+
+
 
 
 def read(filepath):
@@ -150,17 +172,30 @@ def myEncryptMAC(file, key, hkey):
     hashfunc = hmac.HMAC(hkey, hashes.SHA256(), backend = default_backend())
     tag = hashfunc.update(cipherText)
     tag = hashfunc.finalize()
+    '''
     enc = open(encPath, "wb")
     enc.write(cipherText)
+    '''
     return cipherText, iv, tag
 
+def myFileEncryptMAC(filepath):
+    file = read(filepath)
+    key = urandom(keyLen)
+    hkey = urandom(hmacLen)
+    extension = os.path.splitext(filepath)[1]
+    encryptedSet = myEncryptMAC(file, key, hkey)
+    return encryptedSet[0], encryptedSet[1], encryptedSet[2], key, hkey, extension
+
+'''
 def myFileEncryptMAC():
     filePath = folderPath
     file = read(filePath)
     key = urandom(keyLen)
     hkey = urandom(hmacLen)
+    #print(file)
     encryptedSet = myEncryptMAC(file, key, hkey)
     return encryptedSet[0], encryptedSet[1], encryptedSet[2], key, hkey, ext
+'''
 
 def myDecryptMAC(file, key, iv, tag, hkey):
     backend = default_backend()
@@ -225,9 +260,20 @@ def myFileDecrypt(encrypInfo):
     dec.write(decryptedSet)
 
 def main():
-    RSA_Enc_info = myRSAEncrypt()
-    print()
-    RSA_Dec_info = myRSADecrypt(RSA_Enc_info[0],RSA_Enc_info[1],RSA_Enc_info[2],RSA_Enc_info[3],RSA_Enc_info[4])
+    
+    for filename in os.listdir(directory):
+        
+        filepath = directory + filename
+        
+        RSA_Enc_info = myRSAEncrypt(filepath)
+        RSA_Dec_info = myRSADecrypt(RSA_Enc_info[0],RSA_Enc_info[1],RSA_Enc_info[2],RSA_Enc_info[3],RSA_Enc_info[4],RSA_Enc_info[5])
+         
+
+    
+    
+    
+    
+    
     #print(RSA_Enc_info)
     '''
     encrypInfo = None
